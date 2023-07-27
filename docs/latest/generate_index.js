06d1d0eceb2eaf0b29c96f7892880b81c1c23a99
dLocal Technologies <mobile-dev@dlocal.com>
2023-07-27 14:10:20 +0000

    Release v0.2.40

---
+++ docs/latest/generate_index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
+function generateRedirectHTML(version) {
+    const htmlContent = `<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Redirecting...</title>
+        <script>
+            window.onload = function() {
+                var currentUrl = window.location.href;
+                var redirectUrl = currentUrl.replace('latest/index.html', '${version}/documentation/dldirectsdk/');
+                redirectUrl = currentUrl.replace('latest', '${version}/documentation/dldirectsdk/');
+                window.location.href = redirectUrl;
+            };
+        </script>
+    </head>
+    <body>
+        <h1>Redirecting...</h1>
+        <p>If you are not redirected automatically, <a href="https://dlocal.github.io/dlocal-direct-ios-sdk/${version}/documentation/dldirectsdk/">click here</a>.</p>
+    </body>
+    </html>`;
+
+    const fileName = `index.html`;
+    const filePath = path.join(__dirname, fileName);
+
+    fs.writeFile(filePath, htmlContent, (err) => {
+        if (err) {
+            console.error('Error writing HTML file:', err);
+        } else {
+            console.log(`HTML file (${fileName}) with redirect to version ${version} has been generated successfully.`);
+        }
+    });
+}
+
+// Read the version from command-line argument
+const version = process.argv[2];
+
+if (!version) {
+    console.error('Please provide a version number as a command-line argument.');
+} else {
+    generateRedirectHTML(version);
+}

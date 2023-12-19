ba1f84979cd756cf0715bf9e021195c495d5f22d
dLocal Technologies <mobile-dev@dlocal.com>
2023-12-19 12:59:20 +0000

    Release v1.0.1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "DLDirectSDK",
             url: "https://github.com/dlocal/dlocal-direct-ios-sdk/releases/download/v1.0.1/DLDirectSDK-1.0.1.zip",
-            checksum: "fda60521dac9bf3692fc8ee7b318669e9187099a9ed7351c7b249ae3838a500e"
+            checksum: "5e7361a6dc77e53540926ee019b0ad57c6bdadd13d23a1c02285be4915a6ed1c"
         )
     ]
 )
